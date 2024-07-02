9fb9389b46fad9c4f7cf94c7cb727e8e2713ab45
premkumar246 <sanamalapremkumar@gmail.com>
2024-07-02 17:19:25 +0530

    added model training

--- src/Kidney_Disease_Classification_App/pipeline/stage_01_data_ingestion.py
+++ src/Kidney_Disease_Classification_App/pipeline/stage_01_data_ingestion.py
@@ -15,13 +15,13 @@ class DataIngestionPipeline:
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
 
-# if __name__ == '__main__':
-#     try:
-#         logger.info(f">>>>>>> stage :: {STAGE_NAME} started <<<<<<<")
-#         obj = DataIngestionPipeline()
-#         obj.main()
-#         logger.info(f">>>>>>> stage :: {STAGE_NAME} completed <<<<<<< \n\n x========x")
-#     except Exception as e:
-#         logger.exception(e)
-#         raise e
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>>> stage :: {STAGE_NAME} started <<<<<<<")
+        obj = DataIngestionPipeline()
+        obj.main()
+        logger.info(f">>>>>>> stage :: {STAGE_NAME} completed <<<<<<< \n\n x========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
 
